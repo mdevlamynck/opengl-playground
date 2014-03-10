f108ee977b86fc63227973c9d8ec4514bbd202da
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-03-10 17:07:29 +0100

    Now compute light in fragment shader

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,10 +1,20 @@
 #version 330
 
+smooth in vec3 fragPosition;
+smooth in vec3 fragNormal;
 smooth in vec4 fragColor;
+
+uniform mat4 modelView;
+uniform vec4 lightColor;
+uniform vec4 lightPosition;
+uniform vec4 ambiantLightColor;
 
 out vec4 outputColor;
 
 void main()
 {
-	outputColor	= fragColor;
+	vec4	normalLightPosition	= normalize( lightPosition - (modelView * vec4(fragPosition, 1.0))	);
+	float	incidenceAngle		= clamp( dot( modelView * vec4(fragNormal, 0.0), normalLightPosition	), 0, 1	);
+
+	outputColor	= (lightColor * fragColor * incidenceAngle) + (ambiantLightColor * fragColor);
 }
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -6,18 +6,16 @@ layout (location = 2) in vec4 color;
 layout (location = 3) in vec2 uv;
 
 uniform mat4 modelViewProjection;
-uniform mat4 modelView;
-uniform vec4 lightColor;
-uniform vec4 lightPosition;
-uniform vec4 ambiantLightColor;
 
+smooth out vec3 fragPosition;
+smooth out vec3 fragNormal;
 smooth out vec4 fragColor;
 
 void main()
 {
 	gl_Position					= modelViewProjection * vec4(position, 1.0);
 
-	vec4	normalLightPosition	= normalize( lightPosition - (modelView * vec4(position, 1.0))	);
-	float	incidenceAngle		= clamp( dot( modelView * vec4(normal, 0.0), normalLightPosition	), 0, 1	);
-	fragColor					= (lightColor * color * incidenceAngle) + (ambiantLightColor * color);
+	fragPosition				= position;
+	fragNormal					= normal;
+	fragColor					= color;
 }
