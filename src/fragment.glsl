097792943d7e73f97bf70a139eed037d6287c350
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-03-10 17:43:27 +0100

    Change position interpolation from model space to camera space

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-smooth in vec3 fragPosition;
+smooth in vec4 fragPosition;
 smooth in vec3 fragNormal;
 smooth in vec4 fragColor;
 
@@ -13,7 +13,7 @@ out vec4 outputColor;
 
 void main()
 {
-	vec4	normalLightPosition	= normalize( lightPosition - (modelView * vec4(fragPosition, 1.0))	);
+	vec4	normalLightPosition	= normalize( lightPosition - fragPosition	);
 	float	incidenceAngle		= clamp( dot( modelView * vec4(fragNormal, 0.0), normalLightPosition	), 0, 1	);
 
 	outputColor	= (lightColor * fragColor * incidenceAngle) + (ambiantLightColor * fragColor);
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -6,8 +6,9 @@ layout (location = 2) in vec4 color;
 layout (location = 3) in vec2 uv;
 
 uniform mat4 modelViewProjection;
+uniform mat4 modelView;
 
-smooth out vec3 fragPosition;
+smooth out vec4 fragPosition;
 smooth out vec3 fragNormal;
 smooth out vec4 fragColor;
 
@@ -15,7 +16,7 @@ void main()
 {
 	gl_Position					= modelViewProjection * vec4(position, 1.0);
 
-	fragPosition				= position;
+	fragPosition				= modelView * vec4(position, 1.0);
 	fragNormal					= normal;
 	fragColor					= color;
 }
